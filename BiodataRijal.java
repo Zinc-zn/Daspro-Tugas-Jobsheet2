27ee1b0a284db3092e18ed95778afb9e741d1b95
Zinc-zn <riskiafifudin05@gmail.com>
2024-09-05 10:43:33 +0700

    Menambah Bidodata Rijal

--- BiodataRijal.java
+++ BiodataRijal.java
@@ -3,13 +3,13 @@ public class BiodataRijal {
         System.out.println("------------------------------------------------------------");
         System.out.println("-- Biodata Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
         System.out.println("------------------------------------------------------------");
-        System.out.println("Nama : Mochammad Rijal Dzaki Rifki Afifudin");
-        System.out.println("Nim : 244107020240");
-        System.out.println("Kelas : 1H ");
+        System.out.println("Nama     : Mochammad Rijal Dzaki Rifki Afifudin");
+        System.out.println("Nim      : 244107020240");
+        System.out.println("Kelas    : 1H ");
         System.out.println("No Absen : 14");
-        System.out.println("Prodi : D-IV Teknik Infromatika ");
-        System.out.println("No HP : 081252877243");
-        System.out.println("Hobi : Bermain game ");
+        System.out.println("Prodi    : D-IV Teknik Infromatika ");
+        System.out.println("No HP    : 081252877243");
+        System.out.println("Hobi     : Bermain game ");
         
     }
 }
