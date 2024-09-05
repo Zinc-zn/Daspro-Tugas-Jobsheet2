b65a6e4acc7ab01ed1e2b741b484456466ffe40c
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-05 10:21:06 +0700

    data diri yusuf

---
+++ tugas.java
@@ -0,0 +1,15 @@
+public class tugas {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama     :M.Adhitya Yusuf Al-Ayyubi");
+        System.out.println("NIM      :244107020045");
+        System.out.println("Kelas    :TI-1H");
+        System.out.println("No Absen :13");
+        System.out.println("Prodi    :D-4 Teknik Informatika");
+        System.out.println("Alamat   :Bengkulu");
+        System.out.println("No Hp    :085319252270");
+        System.out.println("Hobi     :Bermain Bola");
+    }
+}
