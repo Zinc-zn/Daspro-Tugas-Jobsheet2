c90be338b7074c2c2e502927625b7e7a71db6375
Zinc-zn <riskiafifudin05@gmail.com>
2024-09-05 10:14:40 +0700

    Biodata Rijal

---
+++ BiodataRijal.java
@@ -0,0 +1,7 @@
+Public class BiodataRijal {
+    public  static void main(String[] args){
+        System.out.println("Nama");
+        System.out.println("Nama");
+        System.out.println("Nama");
+    }
+}
